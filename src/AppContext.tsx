8071e8717cd649f2b763ce38581d82c9c594ada4
Davi Gonçalves Valério <al.davigvalerio@gmail.com>
2021-07-27 12:02:30 -0300

    useReducer

--- src/AppContext.tsx
+++ src/AppContext.tsx
@@ -1,11 +1,33 @@
-import React, { FC, useContext } from 'react';
+/* eslint-disable camelcase */
+import React, { FC, useReducer } from 'react';
 
-import CartContext from './CartContext';
+interface Cart {
+  products: string[];
+  shipping_value?: number;
+}
+
+type CartActionType = { type: 'ADD_PRODUCT' | 'REMOVE_PRODUCT' };
 
 const AppContext: FC = () => {
-  const { products } = useContext(CartContext);
+  const cart = useReducer(
+    (state: Cart, action: CartActionType) => {
+      switch (action.type) {
+        case 'ADD_PRODUCT':
+          return {
+            ...state,
+            products: [...state.products, 'Produto novo'],
+          };
+        default:
+          return state;
+      }
+    },
+    {
+      products: [],
+      shipping_value: 0,
+    }
+  );
 
-  return <ul>{products.map((product) => product.name)}</ul>;
+  return <ul>{}</ul>;
 };
 
 export default AppContext;
